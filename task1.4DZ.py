474ea08e9d8abcc0ec920b5329da10ed66d2f306
Mark <masmik@rambler.ru>
2022-09-25 22:40:27 +0300

    DZ 1 lecture

--- task1.4DZ.py
+++ task1.4DZ.py
@@ -1,16 +1,17 @@
 # Напишите программу,
 # которая по заданному номеру четверти
 # показывает диапазон возможных координат точек в этой четверти (x и y)
+
 quarter = int(input('Введите номер четверти: '))
 def GetRange(q):
     if (q == 1):
-         return " X > 0 и Y > 0"
+         return " X > 0 и Y > 0 "
     elif (q == 2):
-         return " X < 0 и Y > 0"
+         return " X < 0 и Y > 0 "
     elif (q == 3):
-         return " X < 0 и Y < 0"
+         return " X < 0 и Y < 0 "
     elif (q == 4):
-         return " X > 0 и Y < 0"
+         return " X > 0 и Y < 0 "
     else:
          return "Введено некоректное значение"
 result = GetRange(quarter)
---
+++ task1.5DZ.py
@@ -0,0 +1,8 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+
+x, y, z = map(float,input('Введите значения X,Y,Z через "," : ').split(","))    
+if (not (x or y or z) == (not x and not y and not z)) == True:
+    print(f"истина")
+else:
+    print(f"ложь")

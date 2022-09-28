970e90a1deab7c27d4fc1a9c75a3cb8275808897
Mark <masmik@rambler.ru>
2022-09-28 03:28:36 +0300

    task2DZ

---
+++ task2.py
@@ -0,0 +1,18 @@
+# a = {1 : 3}
+# a = a.update({2 : 6}) * 3
+# print(a)
+
+# # a = [i**2 for i in range(10)]
+# i = 1
+# b = {i:i**2 for i in range(10)}
+# #cor = int(input())
+# print(b)
+
+n = int(input())
+# l = [i for i in range(-n, n+1)]
+print(l)
+
+
+# n = int(input('Введите число n: '))
+# result = {((1+1/i)**i) for i in range(1, n + 1)}
+# print(f'Сумма чисел: {sum(result)}')

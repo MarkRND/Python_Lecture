1a3dd4923306561a3d507fc533ecdc1bf564d784
Mark <masmik@rambler.ru>
2022-09-28 03:33:48 +0300

    task2.2

--- task2.2.py
+++ task2.2.py
@@ -6,8 +6,8 @@
 #     - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 n = int(input('Введите число N: '))
-b = {i:(3*i+1) for i in range(1,n+1)}
-print(b)
+dictionary= {i:(3*i+1) for i in range(1,n+1)}
+print(dictionary)
 
 
 # a = {1 : 3}
